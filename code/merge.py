2fd4e9b3994c2e620fea71579b38fa95dc040d91
Oluwayetty <oluwayetti@gmail.com>
2019-04-23 19:25:29 +0200

    removed output

--- code/merge.py
+++
@@ -1,9 +0,0 @@
-filenames = [
-             '/Users/oluwayetty1/Desktop/School/1-2/NLP/public_homework_1/simplified_data/training/input/msr_training.txt',
-             '/Users/oluwayetty1/Desktop/School/1-2/NLP/public_homework_1/simplified_data/training/input/as_training.txt']
-
-with open('/Users/oluwayetty1/Desktop/School/1-2/NLP/public_homework_1/data/asmsr/input.txt', 'w+') as outfile:
-    for fname in filenames:
-        with open(fname) as infile:
-            for line in infile:
-                outfile.write(line)
