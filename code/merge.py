3cf88e4f65f963b977e7db33a7d8445a0760b5b1
Oluwayetty <oluwayetti@gmail.com>
2019-04-23 19:11:04 +0200

    removed output

--- code/merge.py
+++ code/merge.py
@@ -1,6 +1,4 @@
 filenames = [
-             # '/Users/oluwayetty1/Desktop/School/1-2/NLP/public_homework_1/simplified_data/training/label/as_training_label.txt',
-             # '/Users/oluwayetty1/Desktop/School/1-2/NLP/public_homework_1/simplified_data/training/label/cityu_training_label.txt',
              '/Users/oluwayetty1/Desktop/School/1-2/NLP/public_homework_1/simplified_data/training/input/msr_training.txt',
              '/Users/oluwayetty1/Desktop/School/1-2/NLP/public_homework_1/simplified_data/training/input/as_training.txt']
 
---
+++ code/split_ngram.py
@@ -0,0 +1,37 @@
+outfile= '/path/to/directory/file'
+infile = '/path/to/directory/file'
+
+def write_file(list):
+    with open(outfile, mode="a") as outfile:
+        for unigram in list:
+            outfile.write(unigram + "\n")
+
+# function to split a file into bigram per line
+def split_into_bigrams(sentence: str):
+    """
+    :param sentence Sentence as str
+    :return bigrams List of bigrams
+    """
+    bigrams = []
+    for i in range(len(sentence)-1):
+        bigram = sentence[i:i+2]
+        bigrams.append(bigram)
+    return bigrams
+
+# function to split a file into unigram per line
+def split_into_unigram(sentence: str):
+    """
+    :param sentence Sentence as str
+    :return bigrams List of bigrams
+    """
+    unigrams = []
+    for i in range(len(sentence)):
+        unigram = sentence[i]
+        unigrams.append(unigram)
+    return unigrams
+
+with open(infile) as file:
+    for line in file:
+        # rstrip() remove '\n' from end of line
+        unigram_list_per_line = split_into_unigram(line.rstrip())
+        write_file(unigram_list_per_line)
